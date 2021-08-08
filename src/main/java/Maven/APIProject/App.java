df2f9ffbd85270e5608bcde8f060e2c0f2e287eb
ashish <ashishsapehia17@gmail.com>
2021-08-08 22:27:42 +0530

    Architect update

--- src/main/java/Maven/APIProject/App.java
+++ src/main/java/Maven/APIProject/App.java
@@ -8,6 +8,13 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	System.out.println( "develop branch" );
+        System.out.println( "Android" );
+        System.out.println( "IOS" );
+        System.out.println( "Native" );
+        
+        int a=9,b = 10;
+        int c = a+b;
+        System.out.println(c);
     }
 }
